604eaffefaa4b55dca17c068c1c9075669f51827
DukulH <dukul.howlader30@gmail.com>
2023-02-20 17:30:14 +0600

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
-echo "Building the project"
-python3.10 -m pip install -r requirment.txt
-
 echo "Make Migration..."
 python3.10 manage.py makemigrations --noinput
 python3.10 manage.py migrate --noinput
