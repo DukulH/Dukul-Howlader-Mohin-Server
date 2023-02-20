407fe13fcb70f715202962bbe363455197cff500
DukulH <dukul.howlader30@gmail.com>
2023-02-20 18:27:06 +0600

    vercel

--- server/settings.py
+++ server/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'agrigate',
-#         'USER' : 'agrigate',
-#         'PASSWORD' : '1498',
-#         'HOST' : 'localhost',
-#         'PORT' : '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agrigate',
+        'USER' : 'agrigate',
+        'PASSWORD' : '1498',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+    }
+}
 DATABASES['default'] = dj_database_url.config()
 
 
