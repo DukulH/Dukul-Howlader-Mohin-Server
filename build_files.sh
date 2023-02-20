f85681558c8d95b9b9ebdc1ebd7304c4d8db3fc8
DukulH <dukul.howlader30@gmail.com>
2023-02-20 18:59:15 +0600

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 echo " BUILD START"
-python3.10 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Build end .."
